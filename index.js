b0f241fc8ae1cc0fa318a099315b98539dae23d3
amaclean <amaclean@evertz.com>
2020-11-13 14:39:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Tone = require('tone');
+
+function play() {
+  const synth = new Tone.Synth().toDestination();
+  const now = Tone.now()
+  synth.triggerAttackRelease("C4", "8n", now)
+  synth.triggerAttackRelease("E4", "8n", now + 0.5)
+  synth.triggerAttackRelease("G4", "8n", now + 1)
+}
+
+//attach a click listener to a play button
+document.querySelector('button')?.addEventListener('click', async () => {
+	await Tone.start()
+	console.log('audio is ready')
+  play()
+})
